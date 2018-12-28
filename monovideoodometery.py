d1e34e186aae464fc6b1bf92363d6ff6782c3309
Ali Shobeiri <alishobeiri@ymail.com>
2018-12-27 19:37:24 -0700

    Sepearated video odometry into it's own class

---
+++ monovideoodometery.py
@@ -0,0 +1,181 @@
+import numpy as np
+import cv2
+import os
+
+
+class MonoVideoOdometery(object):
+    def __init__(self, 
+                img_file_path,
+                pose_file_path,
+                focal_length = 718.8560,
+                pp = (607.1928, 185.2157), 
+                lk_params=dict(winSize  = (21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01)), 
+                detector=cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)):
+        '''
+        Arguments:
+            img_file_path {str} -- File path that leads to image sequences
+            pose_file_path {str} -- File path that leads to true poses from image sequence
+        
+        Keyword Arguments:
+            focal_length {float} -- Focal length of camera used in image sequence (default: {718.8560})
+            pp {tuple} -- Principal point of camera in image sequence (default: {(607.1928, 185.2157)})
+            lk_params {dict} -- Parameters for Lucas Kanade optical flow (default: {dict(winSize  = (21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))})
+            detector {cv2.FeatureDetector} -- Most types of OpenCV feature detectors (default: {cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)})
+        
+        Raises:
+            ValueError -- Raised when file either file paths are not correct, or img_file_path is not configured correctly
+        '''
+
+        self.file_path = img_file_path
+        self.detector = detector
+        self.lk_params = lk_params
+        self.focal = focal_length
+        self.pp = pp
+        self.R = np.zeros(shape=(3, 3))
+        self.t = np.zeros(shape=(3, 3))
+        self.id = 0
+        self.n_features = 0
+
+        try:
+            if not all([".png" in x for x in os.listdir(img_file_path)]):
+                raise ValueError("img_file_path is not correct and does not exclusively png files")
+        except Exception as e:
+            print(e)
+            raise ValueError("The designated img_file_path does not exist, please check the path and try again")
+
+        try:
+            with open(pose_file_path) as f:
+                self.pose = f.readlines()
+        except Exception as e:
+            print(e)
+            raise ValueError("The pose_file_path is not valid or did not lead to a txt file")
+
+        self.process_frame()
+
+
+    def hasNextFrame(self):
+        '''Used to determine whether there are remaining frames
+           in the folder to process
+        
+        Returns:
+            bool -- Boolean value denoting whether there are still 
+            frames in the folder to process
+        '''
+
+        return self.id < len(os.listdir(self.file_path)) 
+
+
+    def detect(self, img):
+        '''Used to detect features and parse into useable format
+
+        
+        Arguments:
+            img {np.ndarray} -- Image for which to detect keypoints on
+        
+        Returns:
+            np.array -- A sequence of points in (x, y) coordinate format
+            denoting location of detected keypoint
+        '''
+
+        p0 = self.detector.detect(img)
+        
+        return np.array([x.pt for x in p0], dtype=np.float32).reshape(-1, 1, 2)
+
+
+    def visual_odometery(self):
+        '''
+        Used to perform visual odometery. If features fall out of frame
+        such that there are less than 2000 features remaining, a new feature
+        detection is triggered. 
+        '''
+
+        if self.n_features < 2000:
+            self.p0 = self.detect(self.old_frame)
+
+
+        # Calculate optical flow between frames, st holds status
+        # of points from frame to frame
+        self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_frame, self.current_frame, self.p0, None, **self.lk_params)
+        
+
+        # Save the good points from the optical flow
+        self.good_old = self.p0[st == 1]
+        self.good_new = self.p1[st == 1]
+
+
+        # If the frame is one of first two, we need to initalize
+        # our t and R vectors so behavior is different
+        if self.id < 2:
+            E, _ = cv2.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+            _, self.R, self.t, _ = cv2.recoverPose(E, self.good_old, self.good_new, self.R, self.t, self.focal, self.pp, None)
+        else:
+            E, _ = cv2.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+            _, R, t, _ = cv2.recoverPose(E, self.good_old, self.good_new, self.R.copy(), self.t.copy(), self.focal, self.pp, None)
+
+            absolute_scale = self.get_absolute_scale()
+            if (absolute_scale):
+                self.t = self.t + absolute_scale*self.R.dot(t)
+                self.R = R.dot(self.R)
+
+        # Save the total number of good features
+        self.n_features = self.good_new.shape[0]
+
+
+    def get_mono_coordinates(self):
+        # We multiply by the diagonal matrix to fix our vector
+        # onto same coordinate axis as true values
+        diag = np.array([[-1, 0, 0],
+                        [0, -1, 0],
+                        [0, 0, -1]])
+        adj_coord = np.matmul(diag, self.t)
+
+        return adj_coord.flatten()
+
+    def get_true_coordinates(self):
+        '''Returns true coordinates of vehicle
+        
+        Returns:
+            np.array -- Array in format [x, y, z]
+        '''
+        return self.true_coord.flatten()
+
+    def get_absolute_scale(self):
+        '''Used to provide scale estimation for mutliplying
+           translation vectors
+        
+        Returns:
+            float -- Scalar value allowing for scale estimation
+        '''
+
+        pose = self.pose[self.id - 1].strip().split()
+        x_prev = float(pose[3])
+        y_prev = float(pose[7])
+        z_prev = float(pose[11])
+        pose = self.pose[self.id].strip().split()
+        x = float(pose[3])
+        y = float(pose[7])
+        z = float(pose[11])
+
+        true_vect = np.array([[x], [y], [z]])
+        self.true_coord = true_vect
+        prev_vect = np.array([[x_prev], [y_prev], [z_prev]])
+        
+        return np.linalg.norm(true_vect - prev_vect)
+
+
+    def process_frame(self):
+        '''Processes images in sequence frame by frame
+        '''
+
+        if self.id < 2:
+            self.old_frame = cv2.imread(self.file_path +str().zfill(6)+'.png', 0)
+            self.current_frame = cv2.imread(self.file_path + str(1).zfill(6)+'.png', 0)
+            self.visual_odometery()
+            self.id = 2
+        else:
+            self.old_frame = self.current_frame
+            self.current_frame = cv2.imread(self.file_path + str(self.id).zfill(6)+'.png', 0)
+            self.visual_odometery()
+            self.id += 1
+
+
--- test.py
+++ test.py
@@ -1,37 +1,12 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+from monovideoodometery import MonoVideoOdometery
 import os
 
-cap = cv.VideoCapture('./videos/test_bronx_trim_1.mp4')
 
-file_path = 'C:\\Users\\Ali\\Desktop\\Projects\\mono-vo\\data_odometry_gray\\dataset\\sequences\\00\\image_0\\'
-annot_path = 'C:\\Users\\Ali\\Desktop\\Projects\\mono-vo\\data_odometry_poses\\dataset\\poses\\00.txt'
-
-with open(annot_path) as f:
-    pose = f.readlines()
-
-def detect(img):
-    p0 = fast.detect(img)
-    p0 = [x.pt for x in p0]
-    
-    return np.array(p0, dtype=np.float32).reshape(-1, 1, 2)
-
-
-def get_absolute_scale(id):
-    ss = pose[id - 1].strip().split()
-    x_prev = float(ss[3])
-    y_prev = float(ss[7])
-    z_prev = float(ss[11])
-    ss = pose[id].strip().split()
-    x = float(ss[3])
-    y = float(ss[7])
-    z = float(ss[11])
-
-    true_vect = np.array([[x], [y], [z]])
-    prev_vect = np.array([[x_prev], [y_prev], [z_prev]])
-    
-    return np.linalg.norm(true_vect - prev_vect)
+img_path = 'C:\\Users\\Ali\\Desktop\\Projects\\SLAM\\videos\\data_odometry_gray\\dataset\\sequences\\00\\image_0\\'
+pose_path = 'C:\\Users\\Ali\\Desktop\\Projects\\SLAM\\videos\\data_odometry_poses\\dataset\\poses\\00.txt'
 
 
 focal = 718.8560
@@ -43,146 +18,36 @@ t_total = np.empty(shape=(3, 1))
 lk_params = dict( winSize  = (21,21),
                   criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
 
-fast = cv.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
-
 
 # Create some random colors
 color = np.random.randint(0,255,(5000,3))
-# Take first frame and find corners in it
-old_gray = cv.imread(file_path +str().zfill(6)+'.png', 0)
-p0 = detect(old_gray)
-frame = cv.imread(file_path + str(1).zfill(6)+'.png', 0)
-frame_gray = frame.copy()
 
+vo = MonoVideoOdometery(img_path, pose_path, focal, pp, lk_params)
+traj = np.zeros(shape=(1000, 1000, 3))
+print(vo.id)
+while(vo.hasNextFrame()):
+    # cv.imshow('frame', vo.current_frame)
+    # k = cv.waitKey(1)
+    # if k == 27:
+    #     break
 
-p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    vo.process_frame()
 
-good_new = p1[st==1]
-good_old = p0[st==1]
+    print(vo.get_mono_coordinates())
 
-E, _ = cv.findEssentialMat(good_new, good_old, focal, pp, cv.RANSAC, 0.999, 1.0, None)
+    mono_coord = vo.get_mono_coordinates()
+    true_coord = vo.get_true_coordinates()
 
-_, R_total, t_total, _ = cv.recoverPose(E, good_old, good_new, R_total, t_total, focal, pp, None)
+    print("MSE Error: ", np.linalg.norm(mono_coord - true_coord))
+    print("x: {}, y: {}, z: {}".format(*[str(pt) for pt in mono_coord]))
+    print("true_x: {}, true_y: {}, true_z: {}".format(*[str(pt) for pt in true_coord]))
 
+    draw_x, draw_y, draw_z = [int(round(x)) for x in mono_coord]
+    true_x, true_y, true_z = [int(round(x)) for x in true_coord]
 
-# color = np.random.randint(0,255,(100,3))
-# print(color)
+    traj = cv.circle(traj, (draw_x + 400, draw_z + 100), 1, list((255, 255, 255)), 1)
+    traj = cv.circle(traj, (true_x + 400, true_z + 100), 1, list((0, 0, 255)), 1)
+    cv.imshow('trajectory', traj)
+cv.imwrite("trajectory.png", traj)
 
-
-good_old = good_new 
-
-n_features = p0.shape[0]
-traj = np.zeros(shape=(600, 600, 3))
-
-true_total = np.array([400, 500])
-mask = np.zeros_like(old_gray)
-count = 2
-flag = True
-for img_id in range(2, len(os.listdir(file_path))):
-    if n_features < 1000:
-        p0 = detect(old_gray)
-        # p0 = cv.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-
-    frame = cv.imread(file_path + str(img_id).zfill(6)+'.png', 0)
-    frame_gray = frame.copy()
-    
-    # frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-    print("p0.shape: ", p0.shape)
-    print("old_gray.shape: ", old_gray.shape)
-    print("frame_gray.shape: ", frame_gray.shape)
-
-    p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-    # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
-    
-    # draw the tracks
-    color = np.random.randint(0,255,(good_old.shape[0],3))
-    p_mask = np.zeros_like(good_new)
-    for i, (new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-
-        if np.linalg.norm(new - old) < 10:
-            p_mask[i] = 1
-
-            if flag:
-                mask = cv.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-                frame = cv.circle(frame,(a,b),5,color[i].tolist(),-1)
-    
-    print(good_old.shape)
-    # Only use good points that don't move too much, ensure points are stable
-    # print("p0: ", p0)
-    # good_new = good_new[p_mask == 1].reshape(-1, 2)
-    # good_old = good_old[p_mask == 1].reshape(-1, 2)
-    E, _ = cv.findEssentialMat(good_new, good_old, focal, pp, cv.RANSAC, 0.999, 1.0, None)
-    # print("E: ", E)
-
-    _, R, t, _ = cv.recoverPose(E, good_old, good_new, R_total.copy(), t_total.copy(), focal, pp, None)
-    # print("R: ", R)
-    print("t: ", t)
-    img = cv.add(frame, mask)
-    cv.imshow('frame', img)
-
-    k = cv.waitKey(1)
-    
-    absolute_scale = get_absolute_scale(img_id)
-    print("absolute_scale: ", absolute_scale)
-    if absolute_scale > 0.1:
-        print("t before: ", t_total)
-        t_total = t_total + absolute_scale*R_total.dot(t)
-        print("t after: ", t_total)
-        R_total = R.dot(R_total)
-        print("R_total: ", R_total)
-    
-
-    draw_x, draw_y = int(round(t_total[0][0]) + 400), int(round(t_total[2][0]) + 500)
-
-
-    ss = pose[img_id].strip().split()
-
-    true_x, true_y = int(round(-float(ss[3])) + 400), int(round(-float(ss[11])) + 500)
-    true_total[0] += true_x
-    true_total[1] += true_y
-    print("True x: ", true_x)
-    print("True z: ", true_y)
-    # if img_id == 4:
-    #     exit()
-    traj = cv.circle(traj, (draw_x, draw_y), 1, list((255, 255, 255)), 1)
-    traj = cv.circle(traj, (true_x, true_y), 1, list((0, 0, 255)), 1)
-    # cv.circle(traj, (true_x,true_y), 1, (0,0,255), 2)
-
-
-
-    print("True total: ", true_x, true_y)
-    # first = (int(round(avg[0])), int(round(avg[1])))
-    # second = (int(round(avg[0] + t[0][0])), int(round(avg[1] + t[1][0])))
-    # t_mask = cv.line(t_mask, first, second, color[i].tolist(), 2)
-
-    # avg[0] += t[0][0]
-    # avg[1] += t[1][0]
-    # avg[2] += t[2][0]
-
-    # # t_mask = cv.line(t_mask, (0,0),(500,500), color[i].tolist(), 2)
-    # traj = cv.add(traj, t_mask)
-    cv.imshow('traj', traj)
-    if k == 27:
-        break
-
-    if k == 121:
-        flag = not flag
-        toggle_out = lambda flag: "On" if flag else "Off"
-        print("Flow lines turned ", toggle_out(flag))
-        mask = np.zeros_like(frame)
-
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
-    n_features = p0.shape[0]
-    count += 1
 cv.destroyAllWindows()
-
-
-# https://github.com/uoip/monoVO-python
-# https://github.com/avisingh599/mono-vo
